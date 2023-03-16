6826043cca335562c9335a1c9efaa254e84c80a8
root <nicymimz@gmail.com>
2023-03-16 13:09:26 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -main function
+ * Return: 1 on success
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

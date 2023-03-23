944c85027c6bae1fbbabcbdd69e40762aa4c5e57
root <nicymimz@gmail.com>
2023-03-23 12:17:28 -0700

    print

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 48; c < 58; c++)
+	for (c = 48 ; c < 58 ; c++)
 	{
 		if (c != 50)
 		{
@@ -19,7 +19,6 @@ void print_most_numbers(void)
 			}
 		}
 	}
-	{
+	
 		_putchar('\n');
-	}
 }

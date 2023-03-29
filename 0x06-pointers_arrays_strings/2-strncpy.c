95ed6cab61bc6f3169b82d9bd8d571c8e35cf104
root <nicymimz@gmail.com>
2023-03-29 03:56:05 -0700

    copying a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strncpy - copys a string
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: dest
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (i = o ; i < n : i++)
+		dest[i] = src[i];
+	return (dest);
+}

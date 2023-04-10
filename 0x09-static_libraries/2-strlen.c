d90afe8977b24633eb664a55861427a8b4d4ab49
root <nicymimz@gmail.com>
2023-04-10 07:28:22 -0700

    creating static library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion -function to get length of string
+ * @s: string to measured
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}

e49c8c884151074900da63b3affccd07258fe38b
root <nicymimz@gmail.com>
2023-05-01 05:13:37 -0700

    print no of elements in a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_listint - print number of elements in a list
+ * @h: element
+ * Return: number of elements
+ */
+
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0 ; h != NULL ; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}

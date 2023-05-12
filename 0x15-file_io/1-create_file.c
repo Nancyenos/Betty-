bca4c5930fba03e0d5810c117cf04cb1091aa632
Nancyenos <nicymimz@gmail.com>
2023-05-12 01:42:39 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,18 +7,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp;
+	int fp, w;
 	int count;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
-	for (count = 0 ; *text_content != '\0' ; count++)
+	if (text_content != NULL)
 	{
-		write(fp, text_content, count);
+		for (count = 0 ; text_content[count] != '\0' ;)
+			count++;
 	}
+	fp = open(filename, O_CREAT |  O_RDWR | O_TRUNC, 0600);
+	{
+		w = write(fp, text_content, count);
+	}
+	if (fp == -1 || w == -1)
+		return (-1);
 	close(fp);
-	return (fp);
+	return (1);
 }
